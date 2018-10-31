ec5bb99c257d6d2e316dd8a858204ff3a2250dc9
kavichelvan <kavichelvan@github.com>
2018-10-31 21:54:24 +0530

    test:module2 complete

--- src/app.js
+++ src/app.js
@@ -12,7 +12,20 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res) => res.render('index',{ title:'Title Index'}));
+const accountData = fs.readFileSync(path.join(__dirname,'json','accounts.json'), 'utf-8');
+const userData = fs.readFileSync(path.join(__dirname,'json','users.json'), 'utf-8');
+const accounts = JSON.parse(accountData);
+const users = JSON.parse(userData);
+
+app.get('/',(req,res) => res.render('index',{ title:'Account Summary',accounts}));
+
+app.get('/savings', (req,res) => res.render('account',{account: accounts.savings}));
+
+app.get('/credit', (req,res) => res.render('account',{account: accounts.credit}));
+
+app.get('/checking', (req,res) => res.render('account',{account: accounts.checking}));
+
+app.get('/profile', (req,res) => res.render('profile',{user: users[0]}));
 
 app.listen(port, () => console.log(`Project running on  ${chalk.yellow('port '+port)}`));
 
