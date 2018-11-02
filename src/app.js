67c7ffa68c826fc7722e3efcd42014ea0ffcff03
Kavichelvan P <41743539+kavichelvan@users.noreply.github.com>
2018-11-02 16:04:17 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -8,26 +8,45 @@ const app = express();
 
 const port = process.env.port || 3000;
 
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync(path.join(__dirname,'json','accounts.json'), 'utf-8');
-const userData = fs.readFileSync(path.join(__dirname,'json','users.json'), 'utf-8');
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf-8');
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf-8');
 const accounts = JSON.parse(accountData);
 const users = JSON.parse(userData);
 
-app.get('/',(req,res) => res.render('index',{ title:'Account Summary',accounts}));
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
 
-app.get('/savings', (req,res) => { res.render('account',{account: accounts.savings})});
+app.get('/savings', (req, res) => { res.render('account', { account: accounts.savings }) });
 
-app.get('/credit', (req,res) => { res.render('account',{account: accounts.credit})});
+app.get('/credit', (req, res) => { res.render('account', { account: accounts.credit }) });
 
-app.get('/checking', (req,res) => { res.render('account',{account: accounts.checking})});
+app.get('/checking', (req, res) => { res.render('account', { account: accounts.checking }) });
 
-app.get('/profile', (req,res) => res.render('profile',{user: users[0]}));
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
-app.listen(port, () => console.log(`Project running on  ${chalk.yellow('port '+port)}`));
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf-8');
+    res.render('transfer', { message: 'Transfer completed'});
+});
 
-    
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname,'json','accounts.json'),accountsJSON,'utf8');
+    res.render('payment', { message: "Payment Successful", account: accounts.credit});
+});
+
+app.listen(port, () => console.log(`Project running on  ${chalk.yellow('port ' + port)}`));
+
+
